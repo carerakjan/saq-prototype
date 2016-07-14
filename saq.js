013a4fc4b52ec53bc4d9e418fa418b9c0c165fea
Bohdan Hryhorian <bohdan.hryhorian@playtech.com>
2016-07-14 16:50:19 +0300

    updated lib

--- saq.js
+++ saq.js
@@ -14,6 +14,10 @@
 
     var log = [];
 
+    var config = {
+        stepTimeout: 30
+    };
+
     var defer = function() {
         return Q.defer();
     };
@@ -32,25 +36,49 @@
         return term + ' ' + measure;
     };
 
-    var test = function(indexOfSuite) {
+    var done = function(deferred) {
+        return function(error) {
+            if(!error) deferred.resolve();
+            else deferred.reject(error);
+        }
+    };
 
-        return function(caseTitle, callback, options) {
+    var startTimer = function(deferred) {
+        setTimeout(function(){
+            deferred.reject('Async callback was not invoked within timeout');
+        }, config.stepTimeout * 1000);
+    };
 
-            options = options || {};
+    var defineStep = function(indexOfSuite) {
+
+        return function(caseTitle, callback) {
 
             log[indexOfSuite].cases.push({
                 suite: indexOfSuite,
                 title: caseTitle,
                 handler: function() {
+                    var deferred = defer();
+                    startTimer(deferred);
+                    callback(done(deferred));
+                    return deferred.promise;
+                }
+            });
 
-                    var args = [].slice.call(arguments);
+        };
 
-                    if(options.async) {
-                        var deferred = defer();
-                        callback.apply(null, [deferred].concat(args));
-                        return deferred.promise;
-                    }
-                    return callback.apply(null, args);
+    };
+
+    var afterAll = function(indexOfSuite) {
+
+        return function(callback) {
+
+            log[indexOfSuite].afterAll.push({
+                skipInReport: true,
+                handler: function () {
+                    var deferred = defer();
+                    startTimer(deferred);
+                    callback(done(deferred));
+                    return deferred.promise;
                 }
             });
 
@@ -62,15 +90,23 @@
 
         var suit = {
             title: suiteTitle,
+            afterAll: [],
             cases: []
         };
 
         log.push(suit);
 
-        callback(test(log.length-1));
+        callback({
+            step: defineStep(log.length-1),
+            afterAll: afterAll(log.length-1)
+        });
     };
 
     var generateReport = function(tests) {
+
+        tests = tests.filter(function(test) {
+            return !test.skipInReport;
+        });
 
         var report = {};
         var isBreak = false;
@@ -82,7 +118,7 @@
             !report[log[test.suite].title] &&
             (report[log[test.suite].title] = {
                 index: test.suite,
-                tests: []
+                steps: []
             });
 
             var t = {title: test.title, result: []};
@@ -95,7 +131,7 @@
                 t.result.push('ok');
             }
 
-            report[log[test.suite].title].tests.push(t);
+            report[log[test.suite].title].steps.push(t);
         });
 
         return report;
@@ -104,17 +140,17 @@
     var run = function() {
 
         var tests = log.reduce(function(test, suite) {
-            return test.concat(suite.cases);
+            return test.concat(suite.cases, suite.afterAll);
         },[]);
 
         return tests.reduce(function(def, test) {
-            !test.execution && (test.execution = []);
+            !test.skipInReport && !test.execution && (test.execution = []);
 
             return def.then(function(){
-                test.execution.push(Date.now());
+                !test.skipInReport && test.execution.push(Date.now());
                 return test.handler.apply(null, arguments);
             }).then(function(data){
-                test.execution.push(Date.now());
+                !test.skipInReport && test.execution.push(Date.now());
                 return data;
             }, function(e) {
 
@@ -122,7 +158,7 @@
                     e = new Error(e);
                 }
 
-                if(!e || (e instanceof Error && !e.message)) {
+                if(!e || !(e instanceof Error) || !e.message) {
                     e = new Error('Unnamed error');
                 }
 
@@ -131,7 +167,7 @@
                     test.error = e.message;
                 }
 
-                test.execution.push(Date.now());
+                !test.skipInReport && test.execution.push(Date.now());
                 throw e;
             });
 
@@ -144,7 +180,28 @@
 
     };
 
+    var setup = function(options) {
+        extend(config, options);
+    };
+
+    var isObject = function(arg) {
+        return typeof arg === 'object' && arg !== null;
+    }
+
+    var extend = function(origin, add) {
+        // Don't do anything if add isn't an object
+        if (!add || !isObject(add)) return origin;
+
+        var keys = Object.keys(add);
+        var i = keys.length;
+        while (i--) {
+            origin[keys[i]] = add[keys[i]];
+        }
+        return origin;
+    }
+
     return {
+        setup: setup,
         suite: suite,
         run: run
     };
